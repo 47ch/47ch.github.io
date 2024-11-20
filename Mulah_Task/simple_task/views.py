33738b00e3d4d225ded6e72c4dea27b1b5fe68bc
Tay Chean Hao <ctay0026@student.monash.edu>
2024-11-20 14:11:50 +0800

    first iteration

--- Mulah_Task/simple_task/views.py
+++ Mulah_Task/simple_task/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def display_tables(request):
+
+    df = pd.read_csv('data/Table_Input.csv')
+    
+    df2 = pd.DataFrame({
+        'City': ['New York', 'Los Angeles', 'Chicago'],
+        'Population': [8000000, 4000000, 2700000]
+    })
+    
+    # Convert DataFrames to HTML tables
+    table1 = df.to_html(classes='table table-striped')
+    table2 = df2.to_html(classes='table table-striped')
+    
+    context = {
+        'table1': table1,
+        'table2': table2
+    }
+    
+    return render(request, 'display_tables.html', context)
