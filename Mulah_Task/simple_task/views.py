3ce2a2c09d5306af994e147439740d34b3ba4a13
Tay Chean Hao <ctay0026@student.monash.edu>
2024-11-20 15:15:39 +0800

    works

--- Mulah_Task/simple_task/views.py
+++ Mulah_Task/simple_task/views.py
@@ -6,10 +6,14 @@ import pandas as pd
 def display_tables(request):
 
     df = pd.read_csv('Table_Input.csv')
+
+    value_1 = int(df.loc[df['Index #'] == 'A5', 'Value']) + int(df.loc[df['Index #'] == 'A20', 'Value'])
+    value_2 = int(df.loc[df['Index #'] == 'A15', 'Value'])/int(df.loc[df['Index #'] == 'A7', 'Value'])
+    value_3 = int(df.loc[df['Index #'] == 'A13', 'Value'])*int(df.loc[df['Index #'] == 'A12', 'Value'])
     
     df2 = pd.DataFrame({
-        'City': ['New York', 'Los Angeles', 'Chicago'],
-        'Population': [8000000, 4000000, 2700000]
+        'Category': ['Alpha', 'Beta', 'Charlie'],
+        'Value': [value_1, value_2, value_3]
     })
     
     # Convert DataFrames to HTML tables
@@ -21,4 +25,5 @@ def display_tables(request):
         'table2': table2
     }
     
-    return render(request, 'display_table.html', context)
+    return render(request, 'simple_task/display_tables.html', context)
+
